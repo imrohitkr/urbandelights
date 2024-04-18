8fc50742fcc4b33ddd956a942d371525aabc4933
pushpakgillhub <117361487+pushpakgillhub@users.noreply.github.com>
2024-04-18 12:09:37 +0530

    Update script.py

--- Backend/script.py
+++ Backend/script.py
@@ -1,7 +1,57 @@
 import sys
+import pickle
+import os
 
-# Read the input text from stdin sent by the Express server
-input_text = sys.stdin.readline().strip()
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
-result = input_text.strip()
-print("of all fruits", result, "is my favorite")
+import keras
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+try:
+    # Read input text from standard input
+    input_text = sys.stdin.readline()
+   
+
+    def classify_review(review_text):
+     # Initialize VADER sentiment analyzer
+        sia = SentimentIntensityAnalyzer()
+    
+        # Analyze sentiment of the review text
+        sentiment_scores = sia.polarity_scores(review_text)
+    
+        # Determine sentiment based on compound score
+        compound_score = sentiment_scores['compound']
+        if compound_score >= 0.05:
+            return "Positive"
+        elif compound_score <= -0.05:
+            return "Negative"
+        else:
+            return "Neutral"
+
+    def main():
+        review_text = input_text
+        sentiment = classify_review(review_text)
+        print("(",sentiment,")")
+
+    if __name__ == "__main__":
+        # Download VADER lexicon if not already downloaded
+        # nltk.download('vader_lexicon')
+        main()
+
+
+
+    if len(input_text) >=250:
+    # Load summarizer model from pickle file
+        with open(r'C:\Users\pushp\Desktop\Project\Backend\summarizer.pkl', 'rb') as f:
+            loaded_summarizer = pickle.load(f)
+
+    # Generate summary using loaded summarizer
+
+        result = loaded_summarizer(input_text)
+        print(result[0]['summary_text'])
+    else:
+        print(input_text)
+
+except Exception as e:
+    print("Error:", e)
